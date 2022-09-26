77bcd85f0c1b771536a7820f25ba4602abad6a98
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-26 12:56:27 +0200

    start-milestone-3

---
+++ ProgramEvents.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class ProgramEvents
+{
+    private string title { get; set; }
+
+    private List<Event> events;
+
+    public ProgramEvents(string title)
+    {
+        this.title = title;
+        events = new List<Event>();
+    }
+}
